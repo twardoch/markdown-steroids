771b57580021d3160dd75ccfccfaef20d23605ca
Adam Twardoch <adam@twardoch.com>
2016-12-23 01:45:25 +0100

    add replimgsrc extension

---
+++ mdx_steroids/replimgsrc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+mdx_steroids.replimgsrc
+======================
+Python Markdown extension.
+Copyright (c) 2016 Adam Twardoch <adam+github@twardoch.com>
+License: [BSD 3-clause](https://opensource.org/licenses/BSD-3-Clause)
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version__ = '0.4.1'
+
+from markdown import Extension
+from markdown.treeprocessors import Treeprocessor
+
+
+class MDXReplaceImageSrcTreeprocessor(Treeprocessor):
+    def __init__(self, md, config):
+        super(MDXReplaceImageSrcTreeprocessor, self).__init__(md)
+        self.config = config
+
+    def run(self, root):
+        imgs = root.getiterator("img")
+        for image in imgs:
+            image.set("src", self.find_replace(image.attrib["src"]))
+
+    def find_replace(self, path):
+        return path.replace(self.config["find"], self.config["replace"])
+
+
+class MDXReplaceImageSrcExtension(Extension):
+    def __init__(self, *args, **kwargs):
+        self.config = {
+            'find'   : ["", "the string to find"],
+            'replace': ["", "the string to replace"],
+        }
+
+        super(MDXReplaceImageSrcExtension, self).__init__(*args, **kwargs)
+
+    def extendMarkdown(self, md, md_globals):
+        ReplaceImageSrc = MDXReplaceImageSrcTreeprocessor(md, self.getConfigs())
+        md.treeprocessors.add(
+            "ReplaceImageSrc",
+            ReplaceImageSrc,
+            "_end"
+        )
+        md.registerExtension(self)
+
+
+def makeExtension(*args, **kwargs):
+    return MDXReplaceImageSrcExtension(*args, **kwargs)
