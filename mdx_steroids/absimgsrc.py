b99df3e1ca46f9c9e1c7354107f6326b8266d4c6
Adam Twardoch <adam@twardoch.com>
2016-12-23 01:31:12 +0100

    new 'absimgsrc' extension

---
+++ mdx_steroids/absimgsrc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+mdx_steroids.absimgsrc
+======================
+Python Markdown extension.
+Copyright (c) 2016 Adam Twardoch <adam+github@twardoch.com>
+License: [BSD 3-clause](https://opensource.org/licenses/BSD-3-Clause)
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version__ = '0.4.1'
+
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
+
+from markdown import Extension
+from markdown.treeprocessors import Treeprocessor
+
+
+class MDXAbsoluteImagesTreeprocessor(Treeprocessor):
+    def __init__(self, md, config):
+        super(MDXAbsoluteImagesTreeprocessor, self).__init__(md)
+        self.config = config
+
+    def run(self, root):
+        imgs = root.getiterator("img")
+        for image in imgs:
+            if self.is_relative(image.attrib["src"]):
+                image.set("src", self.make_external(image.attrib["src"]))
+
+    def make_external(self, path):
+        return urljoin(self.config["base_url"], path)
+
+    def is_relative(self, link):
+        if link.startswith('http'):
+            return False
+        return True
+
+
+class MDXAbsoluteImagesExtension(Extension):
+    def __init__(self, *args, **kwargs):
+        self.config = {
+            'base_url': [None,
+                         "The base URL to which the relative paths will be appended"],
+        }
+
+        super(MDXAbsoluteImagesExtension, self).__init__(*args, **kwargs)
+
+    def extendMarkdown(self, md, md_globals):
+        absoluteImages = MDXAbsoluteImagesTreeprocessor(md, self.getConfigs())
+        md.treeprocessors.add(
+            "absoluteImages",
+            absoluteImages,
+            "_end"
+        )
+        md.registerExtension(self)
+
+
+def makeExtension(*args, **kwargs):
+    return MDXAbsoluteImagesExtension(*args, **kwargs)
