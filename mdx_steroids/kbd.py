5e4a9dca85322d3ab416b8bbe8444f40244f7911
Adam Twardoch <adam@twardoch.com>
2017-02-01 10:42:29 +0100

    kbd

--- mdx_steroids/kbd.py
+++ mdx_steroids/kbd.py
@@ -1,85 +1,72 @@
-"""KBD."""
+"""
+Keys.
+
+pymdownx.keys
+
+MIT license.
+
+Extension to input keys with ease.
+
+Idea by Adam Twardoch and coded by Isaac Muse.
+
+Copyright (c) 2017 Isaac Muse <isaacmuse@gmail.com>
+"""
 from __future__ import unicode_literals
 from markdown import Extension
 from markdown.inlinepatterns import Pattern
+from markdown import util as md_util
 from . import util
 from . import keymap_db as keymap
 import re
 
 RE_KBD = r'''(?x)
-\+{2}
-    # Menu
-    (?::
-        (
-            (?:(?:"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')\+)*?
-            (?:"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')
-        ) |
+(?:
+    # Escape
+    (?<!\\)(?P<escapes>(?:\\{2})+)(?=\+)|
     # Key
-        (
-            (?:(?:[\w\-]+|"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')\+)*?
-            (?:[\w\-]+|"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')
-        )
+    (?<!\\)\+{2}
+    (
+        (?:(?:[\w\-]+|"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')\+)*?
+        (?:[\w\-]+|"(?:\\.|[^"])+"|\'(?:\\.|[^\'])+\')
     )
-\+{2}
+    \+{2}
+)
 '''
-KBD_OUTPUT = '<samp%(class)s>%(name)s</samp>'
-KBD_INPUT = '<kbd%(class)s>%(name)s</kbd>'
-SEP = '<span class="%(class)s%(type)ssep">%(sep)s</span>'
-KBD_WRAP = '<kbd class="%(class)s%(type)s">%(keys)s</kbd>'
+
 ESCAPE_RE = re.compile(r'''(?<!\\)(?:\\\\)*\\(.)''')
+UNESCAPED_PLUS = re.compile(r'''(?<!\\)(?:\\\\)*(\+)''')
+ESCAPED_BSLASH = '%s%s%s' % (md_util.STX, ord('\\'), md_util.ETX)
+DOUBLE_BSLASH = '\\\\'
 
 
-def _escape(txt):
-    """Basic html escaping."""
-
-    txt = txt.replace('&', '&amp;')
-    txt = txt.replace('<', '&lt;')
-    txt = txt.replace('>', '&gt;')
-    txt = txt.replace('"', '&quot;')
-    return txt
-
-
-class KbdPattern(Pattern):
+class KeysPattern(Pattern):
     """Return kbd tag."""
 
     def __init__(self, pattern, config, md):
         """Initialize."""
 
-        ksep = config['keyboard_separator']
-        msep = config['menu_separator']
+        self.ksep = config['separator']
         self.markdown = md
         self.strict = config['strict']
-        self.classes = config['class']
+        self.classes = config['class'].split(' ')
         self.html_parser = util.HTMLParser()
+        self.map = self.merge(keymap.keymap, config['key_map'])
+        self.aliases = keymap.aliases
+        self.camel = config['camel_case']
+        super(KeysPattern, self).__init__(pattern)
 
-        # Keyboard separator
-        if ksep and not self.strict:
-            self.ksep =  SEP % {'class': self.classes + ' ', 'type': 'keyboard ', 'sep': ksep}
-        elif ksep:
-            self.ksep =  SEP % {'class': '', 'type': '', 'sep': ksep}
-        else:
-            self.ksep = ''
+    def merge(self, x, y):
+        """Given two dicts, merge them into a new dict."""
 
-        # Menu separator
-        if msep and not self.strict:
-            self.msep =  SEP % {'class': self.classes + ' ', 'type': 'menu ', 'sep': msep}
-        elif msep:
-            self.msep =  SEP % {'class': '', 'type': '', 'sep': msep}
-        else:
-            self.msep = ''
-
-        # Menu format
-        if self.strict:
-            self.menu = KBD_OUTPUT
-        else:
-            self.menu = KBD_INPUT
-
-        # Keyboard format
-        self.keyboard = KBD_INPUT
-        Pattern.__init__(self, pattern)
+        z = x.copy()
+        z.update(y)
+        return z
 
     def normalize(self, key):
         """Normalize the value."""
+
+        if not self.camel:
+            return key
 
         norm_key = []
         last = ''
@@ -94,98 +81,74 @@ class KbdPattern(Pattern):
             last = c
         return ''.join(norm_key)
 
-    def process_menu(self, key):
-        """Process menu."""
-
-        return (None, _escape(self.html_parser.unescape(ESCAPE_RE.sub(r'\1', key[1:-1]))))
-
     def process_key(self, key):
         """Process key."""
 
         if key.startswith(('"', "'")):
-            value = (None, _escape(self.html_parser.unescape(ESCAPE_RE.sub(r'\1', key[1:-1]))))
+            value = (None, self.html_parser.unescape(ESCAPE_RE.sub(r'\1', key[1:-1])).strip())
         else:
             norm_key = self.normalize(key)
-            canonical_key = keymap.aliases.get(norm_key, norm_key)
-            name = keymap.keymap.get(canonical_key, None)
-            value = (canonical_key, _escape(name)) if name else None
+            canonical_key = self.aliases.get(norm_key, norm_key)
+            name = self.map.get(canonical_key, None)
+            value = (canonical_key, name) if name else None
         return value
 
     def handleMatch(self, m):
         """Hanlde kbd pattern matches."""
 
-        if m.group(3):
-            is_key = True
-            content = [self.process_key(key) for key in m.group(3).split('+')]
-        else:
-            is_key = False
-            content = [self.process_menu(menu) for menu in m.group(2).split('+')]
+        if m.group(2):
+            return m.group('escapes').replace(DOUBLE_BSLASH, ESCAPED_BSLASH)
+        content = [self.process_key(key) for key in UNESCAPED_PLUS.split(m.group(3)) if key != '+']
 
         if None in content:
             return
 
-        html = []
-        base_classes = []
-        if not self.strict:
-            if self.classes:
-                base_classes.append(self.classes)
-            base_classes.append('keyboard' if is_key else 'menu')
+        el = md_util.etree.Element(
+            ('kbd' if self.strict else 'span'),
+            ({'class': ' '.join(self.classes)} if self.classes else {})
+        )
 
+        last = None
         for item_class, item_name in content:
-            classes = base_classes[:]
+            classes = []
             if item_class:
                 classes.append('key-' + item_class)
-            html.append(
-                (self.keyboard if is_key else self.menu) % {
-                    'class': (' class="%s"' % ' '.join(classes)) if classes else '',
-                    'name': item_name
-                }
-            )
+            if last is not None and self.ksep:
+                span = md_util.etree.SubElement(el, 'span')
+                span.text = md_util.AtomicString(self.ksep)
+            attr = {}
+            if classes:
+                attr['class'] = ' '.join(classes)
+            kbd = md_util.etree.SubElement(el, 'kbd', attr)
+            kbd.text = md_util.AtomicString(item_name)
+            last = kbd
 
-        separator = self.ksep if is_key else self.msep
-
-        if self.strict:
-            kbd = KBD_WRAP % {
-                'class': self.classes + ' ' if self.classes else '',
-                'type': 'keyboard' if is_key else 'menu',
-                'keys': separator.join(html)
-            }
-        else:
-            kbd = separator.join(html)
-
-        return self.markdown.htmlStash.store(kbd, safe=True)
+        return el
 
 
-class KbdExtension(Extension):
+class KeysExtension(Extension):
     """Add KBD extension to Markdown class."""
 
     def __init__(self, *args, **kwargs):
         """Initialize."""
 
         self.config = {
-            'keyboard_separator': ['+', "Provide a keyboard separator - Default: \"+\""],
-            'menu_separator': ['\u2192', "Provide a menu separator - Default: \"\u2192\""],
+            'separator': ['+', "Provide a keyboard separator - Default: \"+\""],
             'strict': [False, "Format keys and menus according to HTML5 spec - Default: False"],
-            'class': ['kbd', "Provide class(es) for the kbd elements - Default: kbd"]
+            'class': ['keys', "Provide class(es) for the kbd elements - Default: kbd"],
+            'camel_case': [False, 'Allow camle case conversion for key names PgDn -> pg-dn - Default: False'],
+            'key_map': [{}, 'Additional keys to include or keys to override - Default: {}']
         }
-        super(KbdExtension, self).__init__(*args, **kwargs)
+        super(KeysExtension, self).__init__(*args, **kwargs)
 
     def extendMarkdown(self, md, md_globals):
         """Add support for emojis."""
 
         util.escape_chars(md, ['+'])
-
-        md.inlinePatterns.add(
-            "kbd",
-            KbdPattern(RE_KBD, self.getConfigs(), md),
-            "<escape"
-        )
+        md.inlinePatterns.add("keys", KeysPattern(RE_KBD, self.getConfigs(), md), "<escape")
 
 
-###################
-# Make Available
-###################
 def makeExtension(*args, **kwargs):
     """Return extension."""
 
-    return KbdExtension(*args, **kwargs)
+    return KeysExtension(*args, **kwargs)
