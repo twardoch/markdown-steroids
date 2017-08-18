b1f6059ddd75e689e193f245cb1f7883937d6231
Adam Twardoch <adam@twardoch.com>
2017-08-18 12:55:12 +0200

    kill_tags

--- mdx_steroids/kill_tags.py
+++ mdx_steroids/kill_tags.py
@@ -34,41 +34,69 @@ from markdown.extensions import Extension
 from markdown.postprocessors import Postprocessor
 import lxml.html
 import lxml.html.soupparser
+import lxml.cssselect.LxmlHTMLTranslator
 from bs4 import BeautifulSoup
 import lxml.etree as et
 
 
 class KillTagsPostprocessor(Postprocessor):
 
+    def remove_keeping_tail(self, element):
+        """Safe the tail text and then delete the element"""
+        self._preserve_tail_before_delete(element)
+        element.getparent().remove(element)
+
+    def _preserve_tail_before_delete(self, node):
+        if node.tail:  # preserve the tail
+            previous = node.getprevious()
+            if previous is not None:  # if there is a previous sibling it will get the tail
+                if previous.tail is None:
+                    previous.tail = node.tail
+                else:
+                    previous.tail = previous.tail + node.tail
+            else:  # The parent get the tail as text
+                parent = node.getparent()
+                if parent.text is None:
+                    parent.text = node.tail
+                else:
+                    parent.text = parent.text + node.tail
+
     def normalize_html(self):
         soup = BeautifulSoup(self.html, "html5lib")
-        self.html = soup.encode(formatter=None)
+        self.html = unicode(soup)
 
     def kill_tags(self):
         tree = lxml.html.fromstring(self.html)
+        cx = lxml.cssselect.LxmlHTMLTranslator()
         for kill_tag in self.kill:
-            et.strip_elements(tree, kill_tag, with_tail=False)
+            for el in tree.xpath(cx.css_to_xpath(kill_tag)):
+                self.remove_keeping_tail(el)
         for kill_empty_tag in self.kill_empty:
             for el in tree.xpath(
-                "//{}[not(descendant-or-self::*/text()[normalize-space()]) and not(descendant-or-self::*/attribute::*)]".format(kill_empty_tag)):
-                el.getparent().remove(el)
+                '//{}['
+                    'not(descendant-or-self::*/text()[normalize-space()])' 
+                    ' and not(descendant-or-self::*/attribute::*)'
+                ']'.format(kill_empty_tag)):
+                self.remove_keeping_tail(el)
         self.html = et.tostring(tree, pretty_print=False)
 
     def run(self, html):
         self.html = html
-        self.kill = self.config.get('kill', '').split()
-        self.kill_empty = self.config.get('kill_empty', '').split()
+        self.kill = self.config.get('kill', '')
+        self.kill_empty = self.config.get('kill_empty', '')
         if self.config.get('normalize', False):
             self.normalize_html()
         self.kill_tags()
-        return self.html
+        if self.config.get('normalize', False):
+            self.normalize_html()
+            return self.html
 
 class KillTagsExtension(Extension):
     def __init__(self, *args, **kwargs):
         self.config = {
             'normalize': [False, 'Normalize HTML before processing'],
-            'kill': ['del', 'List of HTML tags to be removed, with contents'],
-            'kill_empty': ['p div h1 h2 h3 h4 h5 h6 details', 'List of HTML tags to be removed if they are empty'],
+            'kill': [['del','code.language-del'], 'List of element CSS selectors to be removed, with contents'],
+            'kill_empty': [['p', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre'], 'List of HTML tags to be removed if they are empty'],
         }
         super(KillTagsExtension, self).__init__(*args, **kwargs)
 
