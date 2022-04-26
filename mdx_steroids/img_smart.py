def630fb30ff6fc7843c5383093e59992e2789fa
twardoch <adam@twardoch.com>
2022-04-27 00:00:08 +0200

    up

--- mdx_steroids/img_smart.py
+++ mdx_steroids/img_smart.py
@@ -12,31 +12,39 @@ from markdown.extensions import attr_list
 from markdown.blockprocessors import BlockProcessor
 from markdown.util import etree
 import PIL, PIL.Image
-#from urlparse import urlparse
+
+# from urlparse import urlparse
 from os.path import splitext, basename, exists
 import requests, io
 
+
 class MDXSmartImageProcessor(BlockProcessor):
-    NOBRACKET = r'[^\]\[]*'
+    NOBRACKET = r"[^\]\[]*"
     BRK = (
-        r'\[(' +
-        (NOBRACKET + r'(\[')*6 +
-        (NOBRACKET + r'\])*')*6 +
-        NOBRACKET + r')\]'
+        r"\[("
+        + (NOBRACKET + r"(\[") * 6
+        + (NOBRACKET + r"\])*") * 6
+        + NOBRACKET
+        + r")\]"
     )
-    NOIMG = r'(?<!\!)'
+    NOIMG = r"(?<!\!)"
     # [text](url) or [text](<url>) or [text](url "title")
-    LINK_RE = NOIMG + BRK + \
-        r'''\(\s*(<.*?>|((?:(?:\(.*?\))|[^\(\)]))*?)\s*((['"])(.*?)\12\s*)?\)'''
+    LINK_RE = (
+        NOIMG
+        + BRK
+        + r"""\(\s*(<.*?>|((?:(?:\(.*?\))|[^\(\)]))*?)\s*((['"])(.*?)\12\s*)?\)"""
+    )
     # ![alttxt](http://x.com/) or ![alttxt](<http://x.com/>)
-    IMAGE_LINK_RE = r'\!' + BRK + r'\s*\(\s*(<.*?>|([^"\)\s]+\s*"[^"]*"|[^\)\s]*))\s*\)'
+    IMAGE_LINK_RE = r"\!" + BRK + r'\s*\(\s*(<.*?>|([^"\)\s]+\s*"[^"]*"|[^\)\s]*))\s*\)'
     # ![alt text][2]
-    IMAGE_REFERENCE_RE = r'\!' + BRK + r'\s?\[([^\]]*)\]'
+    IMAGE_REFERENCE_RE = r"\!" + BRK + r"\s?\[([^\]]*)\]"
 
-
-    FIGURES = [u'^\s*'+IMAGE_LINK_RE+ u'(\{\:?([^\}]*)\})?' + u'\s*$', u'^\s*'+IMAGE_REFERENCE_RE+u'\s*$']
-    INLINE_LINK_RE = re.compile(r'\[([^\]]*)\]\(([^)]+)\)(\{\:?([^\}]*)\})?')
-    FIGURES_RE = re.compile(u'|'.join(f for f in FIGURES))
+    FIGURES = [
+        "^\s*" + IMAGE_LINK_RE + "(\{\:?([^\}]*)\})?" + "\s*$",
+        "^\s*" + IMAGE_REFERENCE_RE + "\s*$",
+    ]
+    INLINE_LINK_RE = re.compile(r"\[([^\]]*)\]\(([^)]+)\)(\{\:?([^\}]*)\})?")
+    FIGURES_RE = re.compile("|".join(f for f in FIGURES))
 
     def __init__(self, md, config):
         super(MDXSmartImageProcessor, self).__init__(md)
@@ -44,8 +52,8 @@ class MDXSmartImageProcessor(BlockProcessor):
 
     def test(self, parent, block):
         isImage = bool(self.FIGURES_RE.search(block))
-        isOnlyOneLine = (len(block.splitlines()) == 1)
-        if (isImage and isOnlyOneLine):
+        isOnlyOneLine = len(block.splitlines()) == 1
+        if isImage and isOnlyOneLine:
             return True
         else:
             return False
@@ -57,21 +65,22 @@ class MDXSmartImageProcessor(BlockProcessor):
         mdImage = self.FIGURES_RE.search(raw_block).group(0)
 
         rAlt = self.FIGURES_RE.search(raw_block)
-        if (rAlt):
+        if rAlt:
             alt = rAlt.group(1)
 
         rUrl = self.INLINE_LINK_RE.search(mdImage)
-        if (rUrl):
+        if rUrl:
             url = rUrl.group(2)
 
         rAttr = self.INLINE_LINK_RE.search(mdImage)
-        if (rAttr):
+        if rAttr:
             attr = self.INLINE_LINK_RE.search(mdImage).group(3)
 
         url_find = self.config.get("find", None)
         url_repl_path = self.config.get("repl_path", None)
         url_repl_url = self.config.get("repl_url", None)
         alt_figure = self.config.get("alt_figure", False)
+        fast_build = self.config.get("fast_build", False)
 
         filepath = url
         orig_url = url
@@ -81,20 +90,20 @@ class MDXSmartImageProcessor(BlockProcessor):
             url = url.replace(url_find, url_repl_url)
 
         width = height = 0
-        img = etree.Element('img')
+        img = etree.Element("img")
         scale = 2.0
         title = None
 
-        if (attr):
+        if attr:
             image_size = self.assignExtra(img, attr)
-            width = int(image_size.get('width', 0))
-            height = int(image_size.get('height', 0))
-            scale = float(image_size.get('scale', 2))
-            title = image_size.get('title', None)
+            width = int(image_size.get("width", 0))
+            height = int(image_size.get("height", 0))
+            scale = float(image_size.get("scale", 2))
+            title = image_size.get("title", None)
 
-        if (width == 0 and height == 0):
+        if width == 0 and height == 0:
             imb = None
-            if filepath.startswith('http'):
+            if filepath.startswith("http") and not fast_build:
                 try:
                     response = requests.get(filepath)
                     imb = io.BytesIO(response.content)
@@ -110,51 +119,51 @@ class MDXSmartImageProcessor(BlockProcessor):
                 except PIL.UnidentifiedImageError:
                     print(f"{filepath} is not an image")
 
-        htmlcls = img.get('class', '')
+        htmlcls = img.get("class", "")
         if htmlcls:
             htmlcls += " image"
         else:
             htmlcls = "image"
         htmlwidth = None
         box = False
-        if (int(width/scale) > 1280):
-            htmlwidth = int(width/scale)
-            htmlcls += ' imxl'
+        if int(width / scale) > 1280:
+            htmlwidth = int(width / scale)
+            htmlcls += " imxl"
             box = True
-        elif (int(width/scale) > 500):
-            htmlwidth = int(width/scale)
-            htmlcls += ' iml'
+        elif int(width / scale) > 500:
+            htmlwidth = int(width / scale)
+            htmlcls += " iml"
             box = True
-        elif (int(width/scale) > 32):
-            htmlwidth = int(width/scale)
-            htmlcls += ' imm'
-        elif (width > 0):
+        elif int(width / scale) > 32:
+            htmlwidth = int(width / scale)
+            htmlcls += " imm"
+        elif width > 0:
             htmlwidth = width
-            htmlcls += ' ims'
+            htmlcls += " ims"
         if htmlwidth:
-            img.set('width', str(htmlwidth))
-        if (alt):
-            img.set('alt', alt)
-        if (htmlcls):
-            img.set('class', htmlcls)
-        img.set('src', orig_url)
+            img.set("width", str(htmlwidth))
+        if alt:
+            img.set("alt", alt)
+        if htmlcls:
+            img.set("class", htmlcls)
+        img.set("src", orig_url)
 
         insel = img
         if box:
-            a = etree.Element('a')
-            a.set('data-fancybox', 'help')
-            a.set('class', 'fancybox')
+            a = etree.Element("a")
+            a.set("data-fancybox", "help")
+            a.set("class", "fancybox")
             if title:
-                a.set('data-caption', title)
+                a.set("data-caption", title)
             elif alt:
-                a.set('data-caption', alt)
-            a.set('href', url)
+                a.set("data-caption", alt)
+            a.set("href", url)
             a.append(img)
             insel = a
-        if (alt and alt_figure):
-            figure = etree.Element('figure')
+        if alt and alt_figure:
+            figure = etree.Element("figure")
             figure.append(insel)
-            figcaption = etree.Element('figcaption')
+            figcaption = etree.Element("figcaption")
             figcaption.text = alt
             figure.append(figcaption)
             insel = figure
@@ -162,44 +171,46 @@ class MDXSmartImageProcessor(BlockProcessor):
 
     # ![By default](/i/ukrainian-keyboard-default.png){: width=400} assign width i.e. <img width="400"/>
     def assignExtra(self, img, attr):
-        image_size = {'width': 0, 'height': 0, 'scale': 2.0}
+        image_size = {"width": 0, "height": 0, "scale": 2.0}
 
-        BASE_RE = r'\{\:?([^\}]*)\}'
-        INLINE_RE = re.compile(r'^%s' % BASE_RE)
-        NAME_RE = re.compile(r'[^A-Z_a-z\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02ff'
-                             r'\u0370-\u037d\u037f-\u1fff\u200c-\u200d'
-                             r'\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff'
-                             r'\uf900-\ufdcf\ufdf0-\ufffd'
-                             r'\:\-\.0-9\u00b7\u0300-\u036f\u203f-\u2040]+')
+        BASE_RE = r"\{\:?([^\}]*)\}"
+        INLINE_RE = re.compile(r"^%s" % BASE_RE)
+        NAME_RE = re.compile(
+            r"[^A-Z_a-z\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02ff"
+            r"\u0370-\u037d\u037f-\u1fff\u200c-\u200d"
+            r"\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff"
+            r"\uf900-\ufdcf\ufdf0-\ufffd"
+            r"\:\-\.0-9\u00b7\u0300-\u036f\u203f-\u2040]+"
+        )
 
         m = INLINE_RE.match(attr)
-        if (m):
+        if m:
             attr = m.group(1)
 
             for k, v in attr_list.get_attrs(attr):
-                if k == '.':
-                    if v == 'lores':
-                        image_size['scale'] = 1.0
-                    htmlcls = img.get('class')
+                if k == ".":
+                    if v == "lores":
+                        image_size["scale"] = 1.0
+                    htmlcls = img.get("class")
                     if htmlcls:
-                        img.set('class', '%s %s' % (htmlcls, v))
+                        img.set("class", "%s %s" % (htmlcls, v))
                     else:
-                        img.set('class', v)
+                        img.set("class", v)
                 else:
-                    if (k == 'data-scale'):
+                    if k == "data-scale":
                         try:
-                            image_size['scale'] = 1/int(v.rstrip("%"))*100
+                            image_size["scale"] = 1 / int(v.rstrip("%")) * 100
                         except ValueError:
                             pass
                     else:
-                        key = NAME_RE.sub('_', k)
+                        key = NAME_RE.sub("_", k)
                         img.set(key, v)
 
-                    if (k == 'width'):
-                        image_size['width'] = v
+                    if k == "width":
+                        image_size["width"] = v
 
-                    if (k == 'height'):
-                        image_size['height'] = v
+                    if k == "height":
+                        image_size["height"] = v
 
         return image_size
 
@@ -207,17 +218,19 @@ class MDXSmartImageProcessor(BlockProcessor):
 class MDXSmartImageExtension(Extension):
     def __init__(self, *args, **kwargs):
         self.config = {
-            'find'   : ["", "the string to find in the URL"],
-            'repl_path': ["", "the string to replace for the local path"],
-            'repl_url': ["", "the string to replace for the final URL"],
-            'alt_figure': [False, "Build <figure> from ![alt]() text"],
+            "find": ["", "the string to find in the URL"],
+            "repl_path": ["", "the string to replace for the local path"],
+            "repl_url": ["", "the string to replace for the final URL"],
+            "alt_figure": [False, "Build <figure> from ![alt]() text"],
+            "fast_build": [False, "Skip slow image processing"],
         }
         super(MDXSmartImageExtension, self).__init__(*args, **kwargs)
 
     def extendMarkdown(self, md, md_globals):
         config = self.getConfigs()
         smartImage = MDXSmartImageProcessor(md.parser, self.getConfigs())
-        md.parser.blockprocessors.add('smartImage', smartImage, '<ulist')
+        md.parser.blockprocessors.add("smartImage", smartImage, "<ulist")
+
 
 def makeExtension(*args, **kwargs):
     return MDXSmartImageExtension(*args, **kwargs)
--- setup.py
+++ setup.py
@@ -11,82 +11,70 @@ except ImportError:
     isPandoc = False
 
 # Get the long description from the README file
-readmepath = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'README.md')
-long_description = ''
+readmepath = os.path.join(os.path.realpath(os.path.dirname(__file__)), "README.md")
+long_description = ""
 if os.path.exists(readmepath):
     if isPandoc:
-        long_description = pandoc(readmepath, read='markdown', write='rst')
+        long_description = pandoc(readmepath, read="markdown", write="rst")
     else:
-        long_description = open(readmepath, encoding='utf-8').read()
+        long_description = open(readmepath, encoding="utf-8").read()
 
 setup(
-    name='mdx_steroids',
-
+    name="mdx_steroids",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.6',
-
-    description='Small collection of Python Markdown extensions',
+    version="0.6.0",
+    description="Small collection of Python Markdown extensions",
     long_description=long_description,
-
     # The project's main homepage.
-    url='https://github.com/twardoch/markdown-steroids/',
-    download_url='https://github.com/twardoch/markdown-steroids/archive/master.zip',
-
+    url="https://github.com/twardoch/markdown-steroids/",
+    download_url="https://github.com/twardoch/markdown-steroids/archive/master.zip",
     # Author details
-    author='Adam Twardoch',
-    author_email='adam+github@twardoch.com',
-
+    author="Adam Twardoch",
+    author_email="adam+github@twardoch.com",
     # Choose your license
-    license='LICENSE',
-
+    license="LICENSE",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Environment :: MacOS X',
+        "Environment :: MacOS X",
         "Environment :: Console",
-        'Operating System :: MacOS :: MacOS X',
+        "Operating System :: MacOS :: MacOS X",
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: Markup',
-        'Topic :: Text Processing :: Markup :: HTML',
-        'Topic :: Software Development :: Documentation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Natural Language :: English',
-
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: English",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-
+        "License :: OSI Approved :: BSD License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 2.7",
     ],
-
     # What does your project relate to?
-    keywords=['Markdown', 'typesetting', 'include', 'plugin', 'extension'],
-
+    keywords=["Markdown", "typesetting", "include", "plugin", "extension"],
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['mdx_steroids'],
-
+    packages=["mdx_steroids"],
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'markdown>=2.6.8',
-        'mako>=1.0.7',
-        'pymdown-extensions>=3.5.0',
-        'cssselect>=1.0.1',
-        'lxml>=3.8.0',
-        'beautifulsoup4>=4.6.0',
+        "markdown>=2.6.8",
+        "mako>=1.0.7",
+        "pymdown-extensions>=3.5.0",
+        "cssselect>=1.0.1",
+        "lxml>=3.8.0",
+        "beautifulsoup4>=4.6.0",
     ],
 )
